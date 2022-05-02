8f781a039a3bf998a14b1a5958d3e917222372c8
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2022-05-02 16:50:49 -0300

    Primeiro dia do NLW
    Criado o widget com TailwindCSS e HeadlessUI

---
+++ web/src/App.tsx
@@ -0,0 +1,24 @@
+import { Widget } from "./components/Widget";
+
+interface ButtonProps{
+  text?: string; //?: faz com que o elemento seja opcional
+}
+//utilizar rem ao contrario de px para questoes de acessibilidade
+function Button(props: ButtonProps){
+  return(
+    <button className="bg-violet-500 px-2 h-7 rounded text-violet-100 hover:bg-violet-800 transition-colors">{props.text ?? 'Default'}</button> //o texto apos ?? se torna o default caso o componente nao envie nada
+  )
+}
+
+export function App() {
+  return (
+    //<div className="flex gap-2"> 
+    //  <h1>Hello world!</h1>
+    //  <Button text="Enviar"/>
+    //  <Button text="Ok"/>
+    //  <Button/>
+    //  </div>
+    <Widget/>
+   
+  )
+}
