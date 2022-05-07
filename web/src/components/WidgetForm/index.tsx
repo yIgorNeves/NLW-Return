674979ffc419caacb76a1db9391b2ee2d8b3c309
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2022-05-06 22:11:20 -0300

    DIA - 5

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -48,7 +48,7 @@ export function WidgetForm() {
     }
 
     return(
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        <div className="bg-zinc-900 p-4 relnative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             { feedbackSent ? (
                 <FeedbackSuceessStep onFeedbackRestartRequested = {handleRestartFeedback}/>
             ): (
